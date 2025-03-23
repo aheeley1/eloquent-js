6fdc9c011ec3dced1cb71995686bf80e35e628af
Adam Heeley <adamheeley@Adams-MacBook-Air.local>
2025-03-23 11:40:15 -0600

    prompt number conversion

--- ref.js
+++ ref.js
@@ -23,3 +23,8 @@ console.log(sum);
 
 product = factorial(8);
 console.log(product);
+
+//Number(), String(), Boolean(), covert a value to that type
+//prompt is not defined in node.js
+//let n = Number(prompt("Enter a number"));
+//console.log(`The square of ${n} is ${n*n}`);
