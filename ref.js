ee1fe06e3038108691c8e8cfe7ac0c2e0f47f05f
Adam Heeley <adamheeley@Adams-MacBook-Air.local>
2025-03-27 14:16:41 -0600

    chained if else

--- ref.js
+++ ref.js
@@ -27,9 +27,23 @@ console.log(product);
 //Number(), String(), Boolean(), covert a value to that type
 //prompt is not defined in node.js
 //conditional execution of code branches
-//let n = Number(prompt("Enter a number"));
-//if (!Number.isNaN(n)){
-    //console.log(`The square of ${n} is ${n*n}`);
-    //console.log(`${n} is the square root of ${n*n}`);
-//}
+// let n = Number(prompt("Enter a number"));
+// if (!Number.isNaN(n)){
+//     console.log(`The square of ${n} is ${n*n}`);
+//     console.log(`${n} is the square root of ${n*n}`);
+// }
+// else {
+//     console.log("Hey, why didn't you give me a number?");
+// }
+
+//chaining conditions
+let n = Number(prompt("Enter a number: "));
+
+if (n < 10){
+    console.log(`${n} is a small number.`);
+} else if (n < 100) {
+    console.log(`${n} is a medium number.`);
+} else {
+    console.log(`${n} is a large number.`);
+}
 
